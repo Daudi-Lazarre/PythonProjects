447eeaa6f984c01bc1f2b10d4c07feee4bfc9844
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2022-04-11 01:22:53 -0400

    Update caesarHacker.py

--- python-CaesarHacker/caesarHacker.py
+++ python-CaesarHacker/caesarHacker.py
@@ -25,7 +25,7 @@ for key in range(len(letters)): # Loop through every possible key
                 num += len(letters)
 
         # Store encrypted number's address
-            translated += letters[num]
+            translated += letter
 
         # Add letter without decrypting.
         else:
